c47882490259f301703ae0f9e99a24129253e2e8
kor_s <kor_s@sujin_2025>
2022-09-21 23:40:42 +0900

    first commit

---
+++ src/day01/basic/MainClass06.java
@@ -0,0 +1,32 @@
+package day01.basic;
+
+/*
+ * [[ 참조형데이터형(레퍼런스형) ]]
+ * String 형
+ * - 선언하는 방법
+ * String 변수명;
+ * - 문자열을 저장할 수 있다.
+ * 
+ *  기본형 8개 - boolean, char, byte, short, int, long, float, double
+ *  
+ *  참조형 - 기본형을 제외한 모든 클래스
+ */
+
+
+
+public class MainClass06 {
+	public static void main(String[] args) {
+		// 변수 선언과 동시에 대입하기
+		String str = "서울 특별시 서대문구 신촌";
+		boolean isContain = str.contains("특별시");
+		if(isContain) {
+			System.out.println("str 에는 특별시 라는 문자열이 있어요!");
+		}
+		System.out.println("4번째 인덱스:"+str.charAt(4));
+		
+		//변수에 저장된 내용을 문자열로 출력해보기
+		System.out.println(str);
+		
+	}
+	
+}
